fc76727c524909429208d53166e78a4cc7af1074
William <william1504@gmail.com>
2021-11-29 18:51:41 -0300

    ok

--- src/hextech-bot/main.py
+++ src/hextech-bot/main.py
@@ -4,23 +4,40 @@ import numpy as np
 from PIL import ImageGrab
 
 from find_command import find_command, mark_command
+from utils.image_processing import prepare_image
 
-x, y = 2665, 100
-w, h = 70, 760
 
+x, y = 2420, 100
+w, h = 61, 760
+offset = 0
+# dt = 700 ms
 
 while True:
-    frame = np.array(ImageGrab.grab(bbox=(x, y, x + w, y + h), all_screens=True))
-    img = cv.cvtColor(frame, cv.COLOR_RGB2BGR)
-    img_gray = cv.cvtColor(img, cv.COLOR_RGB2GRAY)
+    frame = np.array(ImageGrab.grab(bbox=(x, y, x + w * 10, y + h), all_screens=True))
+    frame = cv.cvtColor(frame, cv.COLOR_RGB2BGR)
 
-    cmd = find_command(img_gray, threshold=0.2)
+    # frame = cv.imread('frame.png')
 
-    if cmd:
-        img = mark_command(img, cmd)
-        print(cmd.type)
+    height, width, _ = frame.shape
+    n = (width - offset) // w
 
-    cv.imshow("frame", img)
+    cuts = []
+    for i in range(n):
+        cut = frame[:, w * i + offset: w * (i + 1) + offset, :]
+
+        img_gray = prepare_image(cut)
+        cmd = find_command(img_gray, threshold=0.75)
+
+        if cmd:
+            cut = mark_command(cut, cmd)
+
+        height, width, _ = cut.shape
+        cv.rectangle(cut, (0, 0), (width, height), (0, 0, 255), 2)
+
+        cuts.append(cut)
+
+    frame = np.hstack(cuts)
+    cv.imshow("frame", frame)
 
     if cv.waitKey(1) & 0Xff == ord('q'):
         break
--- src/hextech-bot/utils/command.py
+++ src/hextech-bot/utils/command.py
@@ -1,6 +1,8 @@
 import cv2 as cv
 from enum import Enum
 
+from utils.image_processing import prepare_image
+
 
 class CommandType(Enum):
     NORM_JUMP = 1
@@ -16,21 +18,21 @@ class Command():
     TYPES = [type for type in CommandType]
 
     TEMPLATES = {
-        CommandType.NORM_JUMP: cv.imread('norm_jump_template.png', 0),
-        CommandType.FAST_JUMP: cv.imread('fast_jump_template.png', 0),
-        CommandType.NORM_DOWN: cv.imread('norm_down_template.png', 0),
-        CommandType.FAST_DOWN: cv.imread('norm_down_template.png', 0),
-        CommandType.NORM_BOMB: cv.imread('norm_bomb_template.png', 0),
-        CommandType.FAST_BOMB: cv.imread('fast_bomb_template.png', 0),
+        CommandType.NORM_JUMP: prepare_image(cv.imread('jump_template.png')),
+        CommandType.FAST_JUMP: prepare_image(cv.imread('jump_fast_template.png')),
+        CommandType.NORM_DOWN: prepare_image(cv.imread('down_template.png')),
+        CommandType.FAST_DOWN: prepare_image(cv.imread('down_fast_template.png')),
+        CommandType.NORM_BOMB: prepare_image(cv.imread('bomb_template.png')),
+        CommandType.FAST_BOMB: prepare_image(cv.imread('bomb_fast_template.png')),
     }
 
     MASKS = {
-        CommandType.NORM_JUMP: cv.imread('norm_jump_template.png', 0),
-        CommandType.FAST_JUMP: cv.imread('fast_jump_template.png', 0),
-        CommandType.NORM_DOWN: cv.imread('norm_down_template.png', 0),
-        CommandType.FAST_DOWN: cv.imread('norm_down_template.png', 0),
-        CommandType.NORM_BOMB: cv.imread('norm_bomb_template.png', 0),
-        CommandType.FAST_BOMB: cv.imread('fast_bomb_template.png', 0),
+        CommandType.NORM_JUMP: cv.imread('jump_mask.png', 0),
+        CommandType.FAST_JUMP: cv.imread('jump_fast_mask.png', 0),
+        CommandType.NORM_DOWN: cv.imread('down_mask.png', 0),
+        CommandType.FAST_DOWN: cv.imread('down_fast_mask.png', 0),
+        CommandType.NORM_BOMB: cv.imread('bomb_mask.png', 0),
+        CommandType.FAST_BOMB: cv.imread('bomb_fast_mask.png', 0),
     }
 
     COLOR = {
