929e280d125962acf0075982e954ea87a71e3c55
William <william1504@gmail.com>
2021-12-13 17:48:15 -0300

    on video

--- src/hextech-bot/main.py
+++ src/hextech-bot/main.py
@@ -1,43 +1,122 @@
 import cv2 as cv
-import numpy as np
+import pandas as pd
+
+from time import sleep
+from datetime import datetime
 
 from PIL import ImageGrab
 
-from find_command import find_command, mark_command
+from utils.timer import timefunc
+from utils.command import Command
 from utils.image_processing import prepare_image
 
 
-x, y = 2420, 100
-w, h = 61, 760
-offset = 0
-# dt = 700 ms
+VELOCITY = -211  # pixel/s
+TARGET_X = 420
 
-while True:
-    frame = np.array(ImageGrab.grab(bbox=(x, y, x + w * 10, y + h), all_screens=True))
-    frame = cv.cvtColor(frame, cv.COLOR_RGB2BGR)
+XMIN_1, XMAX_1 = 550, 610
+XMIN_2, XMAX_2 = 1050, 1110
+YMIN, YMAX = 100, 800
 
-    # frame = cv.imread('frame.png')
+CAPTURE = cv.VideoCapture('data/video_sample.mp4')
+FRAME_LIMIT = 1 / 60
 
-    height, width, _ = frame.shape
-    n = (width - offset) // w
+TIME_WINDOW = 0.1
+TIME_OFFSET = 0.0
+START_TIME = datetime.now()
 
-    cuts = []
-    for i in range(n):
-        cut = frame[:, w * i + offset: w * (i + 1) + offset, :]
+queue_df = pd.DataFrame(columns=['Time', 'Command'])
+exec_df = pd.DataFrame(columns=['Frame', 'Command'])
 
-        img_gray = prepare_image(cut)
-        cmd = find_command(img_gray, threshold=0.75)
 
-        if cmd:
-            cut = mark_command(cut, cmd)
+@timefunc(verbose=False)
+def cut_and_process(frame, xmin, xmax, ymin, ymax, current_time):
+    cut = frame[ymin:ymax, xmin:xmax, :]
 
-        height, width, _ = cut.shape
-        cv.rectangle(cut, (0, 0), (width, height), (0, 0, 255), 2)
+    gray = prepare_image(cut)
+    cmd = Command.find(gray, xmin, TARGET_X, VELOCITY, current_time, threshold=0.75)
 
-        cuts.append(cut)
+    if cmd:
+        cut = cmd.mark(cut)
 
-    frame = np.hstack(cuts)
-    cv.imshow("frame", frame)
+    height, width, _ = cut.shape
+    cv.rectangle(cut, (0, 0), (width, height), (0, 0, 255), 2)
 
-    if cv.waitKey(1) & 0Xff == ord('q'):
+    return cmd
+
+
+def add_to_queue(cmd, df):
+    if cmd:
+        return df.append({
+            'Command': cmd.type,
+            'Time': round(cmd.time, 1)
+        }, ignore_index=True)
+
+    return df
+
+
+n = 0
+while CAPTURE.isOpened():
+    # frame = np.array(ImageGrab.grab(bbox=(x, y, x + w * n, y + h), all_screens=True))
+    # frame = cv.cvtColor(frame, cv.COLOR_RGB2BGR)
+
+    ret, frame = CAPTURE.read()
+    if not ret:
+        print('Stream end. Exiting...')
         break
+    n += 1
+
+    # time control
+    now = datetime.now()
+    current_time = (now - START_TIME).total_seconds()
+
+    # look at frame
+    cmd1 = cut_and_process(frame, XMIN_1, XMAX_1, YMIN, YMAX, current_time)
+    cmd2 = cut_and_process(frame, XMIN_2, XMAX_2, YMIN, YMAX, current_time)
+
+    # build queue
+    queue_df = add_to_queue(cmd1, queue_df)
+    queue_df = add_to_queue(cmd2, queue_df)
+
+    queue_df.drop_duplicates(subset=['Time'], inplace=True)
+    queue_df.sort_values(by=['Time'], inplace=True)
+
+    # execute queue
+    TMIN = current_time - TIME_OFFSET - TIME_WINDOW
+    TMAX = current_time - TIME_OFFSET + TIME_WINDOW
+    query = f'Time > {TMIN} & Time < {TMAX}'
+    queue_next = queue_df.query(query)
+    lines = queue_next.shape[0]
+
+    cmd = None
+    if lines > 0:
+        next_line = queue_next.iloc[0]
+        name = next_line.name
+        cmd = next_line['Command']
+
+        queue_next.drop(name)
+        cv.rectangle(frame, (TARGET_X, YMIN), (TARGET_X, YMAX), Command.COLOR[cmd], 2)
+
+    exec_df = exec_df.append({
+        'Frame': n,
+        'Command': cmd,
+    }, ignore_index=True)
+
+    cv.imshow('frame', frame)
+    if cv.waitKey(1) == ord('q'):
+        break
+
+    time_diff = (datetime.now() - now).total_seconds()
+    if (time_diff < FRAME_LIMIT):
+        sleep(FRAME_LIMIT - time_diff)
+
+
+CAPTURE.release()
+cv.destroyAllWindows()
+
+
+exec_df.to_csv('data/queue.csv')
+print(queue_df)
+
+exec_df.to_csv('data/exec.csv')
+print(exec_df)
--- src/hextech-bot/utils/command.py
+++ src/hextech-bot/utils/command.py
@@ -1,4 +1,6 @@
 import cv2 as cv
+import numpy as np
+
 from enum import Enum
 
 from utils.image_processing import prepare_image
@@ -18,37 +20,98 @@ class Command():
     TYPES = [type for type in CommandType]
 
     TEMPLATES = {
-        CommandType.NORM_JUMP: prepare_image(cv.imread('jump_template.png')),
-        CommandType.FAST_JUMP: prepare_image(cv.imread('jump_fast_template.png')),
-        CommandType.NORM_DOWN: prepare_image(cv.imread('down_template.png')),
-        CommandType.FAST_DOWN: prepare_image(cv.imread('down_fast_template.png')),
-        CommandType.NORM_BOMB: prepare_image(cv.imread('bomb_template.png')),
-        CommandType.FAST_BOMB: prepare_image(cv.imread('bomb_fast_template.png')),
+        CommandType.NORM_JUMP: prepare_image(cv.imread('data/jump_template.png')),
+        CommandType.FAST_JUMP: prepare_image(cv.imread('data/jump_fast_template.png')),
+        CommandType.NORM_DOWN: prepare_image(cv.imread('data/down_template.png')),
+        CommandType.FAST_DOWN: prepare_image(cv.imread('data/down_fast_template.png')),
+        CommandType.NORM_BOMB: prepare_image(cv.imread('data/bomb_template.png')),
+        CommandType.FAST_BOMB: prepare_image(cv.imread('data/bomb_fast_template.png')),
     }
 
-    MASKS = {
-        CommandType.NORM_JUMP: cv.imread('jump_mask.png', 0),
-        CommandType.FAST_JUMP: cv.imread('jump_fast_mask.png', 0),
-        CommandType.NORM_DOWN: cv.imread('down_mask.png', 0),
-        CommandType.FAST_DOWN: cv.imread('down_fast_mask.png', 0),
-        CommandType.NORM_BOMB: cv.imread('bomb_mask.png', 0),
-        CommandType.FAST_BOMB: cv.imread('bomb_fast_mask.png', 0),
+    MASK = {
+        CommandType.NORM_JUMP: cv.imread('data/jump_mask.png', 0),
+        CommandType.FAST_JUMP: cv.imread('data/jump_fast_mask.png', 0),
+        CommandType.NORM_DOWN: cv.imread('data/down_mask.png', 0),
+        CommandType.FAST_DOWN: cv.imread('data/down_fast_mask.png', 0),
+        CommandType.NORM_BOMB: cv.imread('data/bomb_mask.png', 0),
+        CommandType.FAST_BOMB: cv.imread('data/bomb_fast_mask.png', 0),
     }
 
     COLOR = {
-        CommandType.NORM_JUMP: (0, 255, 0),
-        CommandType.FAST_JUMP: (0, 255, 0),
-        CommandType.NORM_DOWN: (255, 255, 255),
-        CommandType.FAST_DOWN: (255, 255, 255),
-        CommandType.NORM_BOMB: (255, 0, 0),
-        CommandType.FAST_BOMB: (255, 0, 0),
+        CommandType.NORM_JUMP: (9, 255, 185),
+        CommandType.FAST_JUMP: (9, 255, 185),
+        CommandType.NORM_DOWN: (245, 244, 246),
+        CommandType.FAST_DOWN: (245, 244, 246),
+        CommandType.NORM_BOMB: (247, 243, 24),
+        CommandType.FAST_BOMB: (247, 243, 24),
     }
 
     def __init__(self, type: CommandType, val: float, top_left: tuple, bottom_right: tuple) -> None:
         self.type = type
         self.val = val
+
         self.top_left = top_left
         self.bottom_right = bottom_right
 
-    def getColor(self):
-        return Command.COLOR[self.type]
+        self.color = Command.COLOR[type]
+        self.time = 0
+
+    def calc_time(self, min_x, target_x, velocity, time):
+        x = min_x + (self.top_left[0] + self.bottom_right[0]) / 2
+
+        dx = target_x - x
+        dt = dx / velocity
+
+        self.time = time + dt
+
+    def mark(self, img):
+        cv.rectangle(img, self.top_left, self.bottom_right, self.color, 1)
+        cv.putText(img, f'{self.val:0.2f}', self.top_left, cv.FONT_HERSHEY_SIMPLEX, 0.4, self.color, 1)
+        return img
+
+    @staticmethod
+    def find_template(img, template, mask, method=cv.TM_CCOEFF_NORMED):
+        img = img.copy()
+
+        # Apply template Matching
+        res = cv.matchTemplate(img, template, method) # mask=mask
+        min_val, max_val, min_loc, max_loc = cv.minMaxLoc(res)
+
+        # If the method is TM_SQDIFF or TM_SQDIFF_NORMED, take minimum
+        if method in [cv.TM_SQDIFF, cv.TM_SQDIFF_NORMED]:
+            val = min_val
+            top_left = min_loc
+        else:
+            val = max_val
+            top_left = max_loc
+
+        h, w = template.shape
+
+        bottom_right = (top_left[0] + w, top_left[1] + h)
+        return val, top_left, bottom_right
+
+    @staticmethod
+    def find(img, min_x, target_x, velocity, time, threshold=0.8):
+
+        results_val = []
+        results = []
+
+        for type in Command.TYPES:
+            template = Command.TEMPLATES[type]
+            mask = Command.MASK[type]
+
+            val, top_left, bottom_right = Command.find_template(img, mask, template)
+
+            cmd = Command(type, val, top_left, bottom_right)
+            cmd.calc_time(min_x, target_x, velocity, time)
+
+            results.append(cmd)
+            results_val.append(val)
+
+        max_ind = np.argmax(results_val)
+        result = results[max_ind]
+
+        if result.val > threshold:
+            return result
+
+        return None
